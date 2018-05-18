a0083a61bde8b676798ef9200f2779c4d05fae39
scottieprom <scottieprom123@gmail.com>
2018-05-17 19:56:44 -0700

    CS 3520 Ex2

--- routes/index.js
+++ routes/index.js
@@ -49,3 +49,10 @@ router.post('/getAllOrders', function (request, response) {
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 module.exports = router;
+
+//now processing post
+router.post('/storeData, function(req, res, next) {
+//expecting data variable called order--retrieve value using body-parser
+var value_name = req.body.order  //retrieve the data associated with order
+res.send("order succesfully received: " + value_name);
+});
